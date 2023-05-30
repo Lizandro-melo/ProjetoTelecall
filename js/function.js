920b0826ec7e9560b4982037244591245c9666cb
Lizandro <joselizandro@souunisuam.com.br>
2023-05-30 15:40:57 -0300

    demo v2.0

--- js/function.js
+++ js/function.js
@@ -189,6 +189,5 @@ const painelUsuario = () => {
     })
     buttonSair.addEventListener("click", () => {
         window.open("index.html", "_self");
-        localStorage.removeItem("user")
     })
 }
