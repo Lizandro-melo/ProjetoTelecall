453cb863cca45800757c7c70567b90f805e16ff4
Lizandro <Lizandro.melllo@gmail.com>
2023-08-24 20:24:18 -0300

    build: flash

--- db/connectionDb.class.php
+++ db/connectionDb.class.php
@@ -2,18 +2,26 @@
 
 class DbConnection {
 
-    public string $servername = "localhost";
-    public string $username = "Telecall";
-    public string $password = "Telecall415263";
-
-    public function testeConnection()
-    {
+    private string $servername = "localhost";
+    private string $username = "Telecall";
+    private string $password = "Telecall415263";
+    private string $port = "3306";
+    private PDO $conn;
+    
+    public function __construct(){
         try{
-            $conn = new PDO("mysql:host=$this->servername:3308;dbname=telecall", $this->username, $this->password);
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this -> conn = new PDO("mysql:host=$this->servername:$this->port;dbname=telecall", $this -> username, $this -> password);
             return "Connected successfully";
         } catch(Error){
             return "Connected Failed";
+        }
+    }
+
+    public function setCliente(){
+        //InserindoDados
+        if(isset($_POST['nome'])){
+            $sql = $this -> conn -> prepare("INSERT INTO telecall VALUES(null,?,?,?,?,?,?,?,?,?,?)");
+            $sql -> execute(array($_POST['nome'],$_POST['sobrenome'],$_POST['cpf'],$_POST['data'],$_POST['telefoneCelular'],$_POST['telefoneFixo'],$_POST['nomeMaterno'],$_POST['endereco'],$_POST['login'],$_POST['senha']));
         }
     }
 
