9c1977243ff9449dabc1a2efcc318a0abf8a3cb1
Lizandro <Lizandro.melllo@gmail.com>
2023-08-24 20:23:30 -0300

    build: flash

--- models/cliente.class.php
+++ models/cliente.class.php
@@ -11,15 +11,37 @@ class Cliente{
     private string $senha;
 
     public function __construct(DbConnection $db) {
-        $this->nome = ;
-        $this->sobrenome = ;
-        $this->cpf = ;
-        $this->dataNascimento = ;
-        $this->telefoneCelular = ;
-        $this->login = ;
-        $this->senha = ;
+        
     }
 
+    public function __getNome()
+    {
+        return $this->nome;
+    }
+    public function __getSobrenome()
+    {
+        return $this->sobrenome;
+    }
+    public function __getCpf()
+    {
+        return $this->cpf;
+    }
+    public function __getDataNascimento()
+    {
+        return $this->dataNascimento;
+    }
+    public function __getTelefoneCelular()
+    {
+        return $this->telefoneCelular;
+    }
+    public function __getLogin()
+    {
+        return $this->login;
+    }
+    public function __getSenha()
+    {
+        return $this->senha;
+    }
 }
 
 
